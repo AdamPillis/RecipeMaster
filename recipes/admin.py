730b5a0f978e953d27b9c70c713301596e71b6a4
AdamPillis <adam.pillis@hotmail.com>
2022-09-06 11:12:20 +0000

    Create superuser and style admin panel

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    """To customise cateogry view via admin panel"""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+    search_fields = ['friendly_name']
+    # use summernote_fields = ('content') for all textfield (instructions in each recipe)
+    # use list_filter = ('', 'created_on')
+
+
+admin.site.register(Category, CategoryAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Class to categorise RecipeMaster's desserts
+    used in developement like search/filter functionality
+    friendly_name is visual to the user instead of the name
+    """
+    class Meta:
+        """To fix plural issue relating to django"""
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """returns category name as string"""
+        return self.name
+
+    def get_friendly_name(self):
+        """returns friendly name of category"""
+        return self.friendly_name
+
+
+# for image field, install pillow before migrations
