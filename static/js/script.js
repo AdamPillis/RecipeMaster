f5ee7eb5f0647e3c744d377a6e472e191d298f7c
AdamPillis <adam.pillis@hotmail.com>
2023-01-11 13:32:27 +0000

    Add click listener to search button to open search input in base.html, base.css and script.js

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,15 @@ setTimeout(function() {
     let alert= new bootstrap.Alert(messages);
     alert.close();
 }, 3000);
+
+const trigger_search = document.querySelector('.trigger_search');
+const submit_button = document.querySelector('.submit_button');
+const input = document.querySelector('.input');
+
+trigger_search.addEventListener('click', () =>{
+    if (input.style.display = 'none') {
+        input.style.display = 'unset';
+        trigger_search.classList.add('input');
+        submit_button.classList.remove('input');
+    }
+})
