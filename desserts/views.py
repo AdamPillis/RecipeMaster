fe0c5cbc975bdbc1d9f1158ac339ea768303f481
AdamPillis <adam.pillis@hotmail.com>
2023-01-13 13:23:44 +0000

    Add: recipes.html, url path and function to desserts app

--- desserts/views.py
+++ desserts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Recipe
 
-# Create your views here.
+
+def all_desserts(request):
+    """
+    Renders products.html and displays
+    all products on website. Search and
+    filter option will also be included.
+    """
+    recipes = Recipe.objects.all()
+
+    context = {
+        'recipes': recipes,
+    }
+
+    return render(request, 'recipes/recipes.html', context)
