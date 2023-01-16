c8449da4d41dfede177f9e43530af211ba29bdbf
AdamPillis <adam.pillis@hotmail.com>
2023-01-16 14:13:35 +0000

    Fix bug related wrong url path for selected category

--- desserts/views.py
+++ desserts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.db.models import Q
+from django.contrib import messages
 from django.db.models.functions import Lower
 
 from .models import Recipe, Category
@@ -13,8 +14,28 @@ def all_desserts(request):
     """
     recipes = Recipe.objects.all()
     query = None
+    categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                recipes = recipes.annotate(lower_name=Lower('name'))
+                # sort by name rather than id, using double under score to
+                # gain access into related object model
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            recipes = recipes.order_by(sortkey)
         # splitting into list after commas and filter list who's
         # category name is in the list
         if 'category' in request.GET:
@@ -28,8 +49,10 @@ def all_desserts(request):
         if 'search' in request.GET:
             query = request.GET['search']
             if not query:
-                messages.error(
+                print('not found')
+                messages.add_message(
                     request,
+                    messages.ERROR,
                     "No search criteria recognized, please try again.")
                 return redirect(reverse('recipes'))
 
@@ -37,9 +60,13 @@ def all_desserts(request):
                 step_guide__icontains=query)
             recipes = recipes.filter(queries)
 
+    chosen_sorting = f'{sort}_{direction}'
+
     context = {
         'recipes': recipes,
         'search_word': query,
+        'chosen_categories': categories,
+        'chosen_sorting': chosen_sorting,
     }
 
     return render(request, 'recipes/recipes.html', context)
