1b47ea484e11bb953a683739356c931328b2cec1
AdamPillis <adam.pillis@hotmail.com>
2023-01-23 14:11:36 +0000

    Working on submit quantity form in full_recipe.html and script.js in static

--- static/js/script.js
+++ static/js/script.js
@@ -16,3 +16,39 @@ trigger_search.addEventListener('click', () =>{
         submit_button.classList.remove('input');
     }
 })
+
+
+//Calling the form element to collect player name and use within handleSubmit function
+let form = document.getElementById('quantity-form');
+form.elements.quantity.focus(); // focusing on text box
+/**
+ * adds Enter event listener to form box by pressing 'Enter' or clicking 'Submit'
+ */
+form.addEventListener('keydown', function(event) {
+
+    if (event.key === 'Enter') {
+        handleSubmit(event);
+    }
+});
+form.addEventListener('submit', handleSubmit);
+
+
+/**Function to collect playerName from form section in #start-screen.
+ * Hides start-screen and opens menu-screens.
+ * PlayerName added to welcome message. 
+ */
+function handleSubmit(event) {
+
+    event.preventDefault();
+
+    let quantity = form.elements.quantity;
+    let ingredientScreen = document.getElementById('ingredient-screen');
+    let quantityScreen = document.getElementById('quantity-screen');
+    let ingredient_quantity = document.getElementsByClassName('quantity-amount');
+
+    console.log(quantity.value)
+    console.log(ingredient_quantity.value)
+
+     quantityScreen.style.display = 'none';
+     ingredientScreen.style.display = 'unset';
+}
